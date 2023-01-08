897faa643038e5592925e512fdb7f62eb4143534
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-09 01:30:04 +0600

    theme added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import "./App.css";
-import Navbar from "./shared/Navbar/Navbar";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+
+const ScrollToTop = () => {
+  const { pathName } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathName]);
+
+  return null;
+};
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
