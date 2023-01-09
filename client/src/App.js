9ba6c6ba4561331991301f9075bd9de1cc143362
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-10 00:56:42 +0600

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "./scenes/home/Home";
+import Checkout from "./pages/checkout/Checkout";
+import Confirmation from "./pages/checkout/Confirmation";
+import Home from "./pages/home/Home";
+import ItemDetails from "./pages/itemDetails/ItemDetails";
+import Navbar from "./pages/shared/Navbar";
 
 const ScrollToTop = () => {
   const { pathName } = useLocation();
@@ -16,9 +20,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,80 +1,75 @@
 import { createTheme } from "@mui/material/styles";
 
+export const Shades = {
+  primary: {
+    100: "#cccccc",
+    200: "#999999",
+    300: "#666666",
+    400: "#333333",
+    500: "#000000",
+    600: "#000000",
+    700: "#000000",
+    800: "#000000",
+    900: "#000000",
+  },
 
+  secondary: {
+    100: "#f7ccd2",
+    200: "#ef99a4",
+    300: "#e66677",
+    400: "#de3349",
+    500: "#d6001c",
+    600: "#ab0016",
+    700: "#800011",
+    800: "#56000b",
+    900: "#2b0006",
+  },
 
-const export Shades = {
-    primary: {
-        100: "#cccccc",
-        200: "#999999",
-        300: "#666666",
-        400: "#333333",
-        500: "#000000",
-        600: "#000000",
-        700: "#000000",
-        800: "#000000",
-        900: "#000000"
-    },
-    
-    secondary: {
-        100: "#f7ccd2",
-        200: "#ef99a4",
-        300: "#e66677",
-        400: "#de3349",
-        500: "#d6001c",
-        600: "#ab0016",
-        700: "#800011",
-        800: "#56000b",
-        900: "#2b0006"
-    },
-    
-
-    neutral: {
-        100: "#f5f5f5",
-        200: "#ecebeb",
-        300: "#e2e1e1",
-        400: "#d9d7d7",
-        500: "#cfcdcd",
-        600: "#a6a4a4",
-        700: "#7c7b7b",
-        800: "#535252",
-        900: "#292929"
-    },
-    
+  neutral: {
+    100: "#f5f5f5",
+    200: "#ecebeb",
+    300: "#e2e1e1",
+    400: "#d9d7d7",
+    500: "#cfcdcd",
+    600: "#a6a4a4",
+    700: "#7c7b7b",
+    800: "#535252",
+    900: "#292929",
+  },
 };
 
 export const theme = createTheme({
-    palette: {
-        primary: {
-            main: Shades.primary[500]
-        },
-        secondary: {
-            main: Shades.secondary[500]
-        },
-        neutral: {
-            dark: Shades.neutral[700],
-            main: Shades.neutral[500],
-            light: Shades.neutral[100]
-        }
+  palette: {
+    primary: {
+      main: Shades.primary[500],
     },
-    typography: {
-        fontFamily: ["Fauna One", "Sans-serif"].join(","),
-        fontSize: 11,
-        h1: {
-            fontFamily: ["Cinzel", "Sans-serif"].join(","),
-        fontSize: 48,
-        },
-        h2: {
-            fontFamily: ["Fauna One", "Sans-serif"].join(","),
-        fontSize: 36,
-        },
-        h3: {
-            fontFamily: ["Fauna One", "Sans-serif"].join(","),
-        fontSize: 20,
-        },
-        h4: {
-            fontFamily: ["Fauna One", "Sans-serif"].join(","),
-        fontSize: 14,
-        },
-        
-    }
-})
+    secondary: {
+      main: Shades.secondary[500],
+    },
+    neutral: {
+      dark: Shades.neutral[700],
+      main: Shades.neutral[500],
+      light: Shades.neutral[100],
+    },
+  },
+  typography: {
+    fontFamily: ["Fauna One", "Sans-serif"].join(","),
+    fontSize: 11,
+    h1: {
+      fontFamily: ["Cinzel", "Sans-serif"].join(","),
+      fontSize: 48,
+    },
+    h2: {
+      fontFamily: ["Fauna One", "Sans-serif"].join(","),
+      fontSize: 36,
+    },
+    h3: {
+      fontFamily: ["Fauna One", "Sans-serif"].join(","),
+      fontSize: 20,
+    },
+    h4: {
+      fontFamily: ["Fauna One", "Sans-serif"].join(","),
+      fontSize: 14,
+    },
+  },
+});
