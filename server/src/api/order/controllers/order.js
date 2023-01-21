8bca4ec5651413c1753ff845928106d107c6bf16
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-01-21 19:18:02 +0600

    stripe payment

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ stripe }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+
+    try {
+      // retrieve item information
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+          return {
+            price_data: {
+              currency: "bdt",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+    } catch (error) {}
+  },
+}));
